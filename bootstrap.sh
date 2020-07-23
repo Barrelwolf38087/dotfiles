f509de827188f5166b7e11b12161b79f3b4c3795
Will Leverone <Barrelwolf38087@gmail.com>
2020-07-23 16:53:36 -0400

    Some more stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,23 +5,12 @@
 DOTFILES=true
 PACKAGES=true
 
-read -r -d '' COPY_LIST << EOM
-bg.png
-.config
-.fehbg
-scripts/clean-orphans
-scripts/i3-get-window-criteria
-scripts/enable-auto-pywal
-.vimrc
-.Xresources
-.zshrc
-EOM
-
 read -r -d '' PACMAN_LIST << EOM
 base-devel
 dhcpcd
 git
 go
+xorg
 i3
 dmenu
 picom
@@ -39,13 +28,19 @@ polybar-spotify-git
 siji-git
 EOM
 
-# Shitty hack, *please* tell me a better way to do this.
-if [ "$1" = "postelevate" ]; then install_packages; fi
 
 dotfiles() {
-    echo "$COPY_LIST" | while IFS= read -r line; do
-        test -f "$HOME/$line" && mv "$HOME/$line" "$HOME/$line.orig" && echo "Backing up old ~/$line"
-        cp -r "$line" "$HOME"
+#    echo "$COPY_LIST" | while IFS= read -r line; do
+#        test -f "$HOME/$line" && mv "$HOME/$line" "$HOME/$line.orig" && echo "Backing up old ~/$line"
+#        cp -r "$line" "$HOME"
+#    done
+
+    echo "Installing dotfiles..."
+    
+    cd "$0/../home"
+    find | sed '/^\.$/d' | sed 's|\./||' | while read f; do
+        test -f "$HOME/$f" && echo "Existing $f found, backing it up..." && mv "$HOME/$f" "$HOME/$f.old"
+        cp -v "$f" "$HOME/$f"
     done
 }
 
@@ -53,6 +48,7 @@ install_packages() {
     if [ ! "$UID" -eq 0 ] && [ "$1" != "--noelevate" ] && [ "$2" != "--noelevate"]; then
         echo "Root required to install packages"
         echo "If your system is configured to not require root to install packages (why?), run this script with --noelevate"
+        echo "$(whoami)" > "$0.username" # Another stupid hack so we can de-elevate for the yay call
         sudo /bin/sh "$0" "postelevate" "$@"
     fi 
 
@@ -66,11 +62,25 @@ install_packages() {
 
     makepkg --noconfirm -si
 
+    # ihateitihateitihateitihateit
+    if [ "$UID" -eq 0 ]; then
+        su "$(cat $0.username)" -c "/bin/sh $0 preyay $@"
+    fi
+}
+
+install_aur_packages() {
+    if [ "$1" = "preyay" ]; then rm "$0.username"; fi
+    
     echo "Installing AUR packages..."
     yay --noconfirm --answerclean=None --answerdiff=None --answeredit=None -S $(echo "$AUR_LIST" | xargs)
 
     echo "Done!"
+    exit
 }
+
+# Shitty hack, *please* tell me a better way to do this.
+if [ "$1" = "postelevate" ]; then install_packages; fi
+if [ "$1" = "preyay" ]; then install_aur_packages; fi
 
 if [ ! command -v pacman &> /dev/null ]; then
     echo "It looks like this isn't an Arch-based distro. Packages will not be installed."
@@ -81,4 +91,4 @@ if [ ! command -v pacman &> /dev/null ]; then
 fi
 
 if [ $DOTFILES ]; then copy_dotfiles; fi
-if [ $PACKAGES ]; then install_packages; fi 
+if [ $PACKAGES ]; then install_packages; install_aur_packages; fi 
